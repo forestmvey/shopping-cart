624cf50030c95807a021ad4148279da5e6de3b17
Forest <forestmvey@gmail.com>
2018-05-29 12:13:36 -0700

    added category adding by product id

--- web/addproductvars.php
+++ web/addproductvars.php
@@ -11,23 +11,28 @@ $category = $_POST['category'];
 $dimensions = $_POST['dimensions'];
 $photo = $_POST['photo'];
 $name = $_POST['name'];
+
+
 //echo "$price $category $dimensions $photo $photoname";
 // connect to database
 
 include('mysqli_connect.php');
 //CREATE QUERY
+
 $insert1 = "INSERT INTO product (name, size, image, price) 
 values('$name', '$dimensions', '$photo', '$price')";
-$insert2 = "INSERT INTO productcategory (category_id) 
-values('$category')";
-//echo $insert1;
+
 // run the query???
 //$row = @mysqli_query ($dbc, $insert)
 // Check the result
-if ((mysqli_query($dbc, $insert1)) && (mysqli_query($dbc, $insert2))) {
+if (mysqli_query($dbc, $insert1)) {
+	$last_id = mysqli_insert_id($dbc);
+	$insert2 = "INSERT INTO productcategory (product_id, category_id) 
+	values('$last_id','$category')";
+	mysqli_query($dbc, $insert2);
 	echo "Insert successful";
 }else { //not a match
-	echo "Error" . $insert1 . "<br>" . mysqli_error($insert1);
+	echo "Error" . $insert1 . "<br>" . mysqli_error($insert1) . mysqli_error($insert2);
 }
 
 
