b086c36107a78ab8fb6e440a456fd77f8ab1cad1
David Hicks <davidhicks14@gmail.com>
2018-06-04 14:37:52 -0700

    fixed logout.php

--- web/logout.php
+++ web/logout.php
@@ -1,5 +1,6 @@
 
 <script type="text/javascript">
+// Popup window which confirms that the session is over
 alert ('You are now logged out');
 window.location="index.php";
 </script>
@@ -14,26 +15,11 @@ window.location="index.php";
 </head>
 <body>
 <?php
+// Destroy session data, resets admin privileges
 session_start();
 session_destroy();
-unset($_SESSION['userid'];
-unset($_SESSION['user'];
-$_SESSION['adminprivilege'] = false;
-echo "You have successfully logged out";
-echo "<h3>To log back in: <a href='login_register.php'>Login</a></h3>";
 ?>
 <footer class="footer">Copyright &copy;2018</footer>
-<?php
-	// This checks if the admin is logged in and allows them to 
-    // add products to the database on when the admin is logged in
-    // and is disabled when the admin is logged out
-    if(isset($_SESSION['adminprivilege'])){
-        echo "<script>";
-        echo "document.getElementById('addprod').style.visibility = 'visible';";
-        echo "</script>";
-   
-    }
 
-?>
 </body>
 </html>
