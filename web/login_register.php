db68ba8237da1e95e82158b5cb0a2f991c74b142
David Hicks <davidhicks14@gmail.com>
2018-06-05 12:21:19 -0700

    final for week4 after demo

--- web/login_register.php
+++ web/login_register.php
@@ -23,9 +23,9 @@
             <li><a href="index.php" title="Main page">Main</a></li>
             <li><a href="photos.php" title="Photos">Photos</a></li>
             <li><a href="cart.php" title="Cart">View Cart</a></li>
-            <li><a href="login_register.php" title="LoginRegister" class="viewing" id="loginregister">Login/Register</a></li>
-			<li><a href="myaccount.php" style="visibility:hidden; title="MyAccount" id="myaccount">My Account</a></li>
-            <li><a href="logout.php" style="visibility:hidden; title="Logout" id=="logout">Logout</a></li>
+            <li><a href="login_register.php" title="LoginRegister" class="viewing">Login/Register</a></li>
+			<li><a href="myaccount.php" id="myaccount" style="visibility:hidden;" title="MyAccount">My Account</a></li>
+			<li><a href="logout.php" id="logout" style="visibility:hidden;" title="Logout">Logout</a></li>
             <li><a href="addproduct.php" id="addprod" style="visibility:hidden;" title="AddProduct">Add Product</a></li>
         </ul>
     </nav>
@@ -52,8 +52,8 @@
 	<form action="register.php" method="POST" onsubmit="return registervalidation();">
     <h1>Register: </h1>
     <p>Email: <input type="text" name="registeremail" pattern="[^@]+@[^@]+\.[a-zA-Z]{2,6}" id="registeremail" title="email"></p>
-    <p>Name: <input type="text" id="name" name="name" title="name"></p>
-    <p>Address: <input type="text" id="address" name="address" title="address"></p>
+    <p>Name: <input type="text" id="name" name="name" title="name" pattern="[\sa-zA-Z]{2,15}"></p>
+    <p>Address: <input type="text" id="address" name="address" title="address" pattern="[\sa-zA-Z0-9]{4,20}"></p>
     <p>Password: <input type="password" name="registerpassword" pattern="[a-zA-Z0-9]{4,10}" id="registerpassword" title="4 to 10 characters letters and numbers only"></p>
     <p>Confirm Password: <input type="password" name="confirmpassword" pattern="[a-zA-Z0-9]{4,10}" id="confirmpassword" title="4 to 10 characters letters and numbers only"></p>
     <input type="submit" value="SUBMIT"  />
@@ -104,13 +104,16 @@
         echo "<script>";
         echo "document.getElementById('addprod').style.visibility = 'visible';";
         echo "</script>";
+   
     }
 	if(isset($_SESSION['user'])){
+	//displays logout and my account only when user is signed in
 		echo "<script>";
         echo "document.getElementById('logout').style.visibility = 'visible';";
 		echo "document.getElementById('myaccount').style.visibility = 'visible';";
         echo "</script>";
 	}
+
 
 ?>
 </body>
