65c93815070c367d5e300691dfd3b5ee2887bb41
Devon <devon1616@hotmail.com>
2018-06-18 11:42:06 -0700

    Updated write_receipt. Remember to change directory to your own and set permissions!

--- web/write_receipt.php
+++ web/write_receipt.php
@@ -2,6 +2,7 @@
 session_start();
 include ('connection.php');
 
+// Setting up variables and timezone
 date_default_timezone_set("America/Vancouver");
 $userid= $_SESSION['userid'];
 $user= $_SESSION['user'];
@@ -9,12 +10,14 @@ $date = date("Y-m-d");
 $time = date("h:i:sa");
 $title = $userid . "_" . $user . "_" . $date . "_" . $time;
 
-
+// Database query
 $receiptInfo = "SELECT * FROM cart c, product p WHERE p.id = c.product_id AND c.customer_id = '$userid'";
 $receiptQuery = mysqli_query($link, $receiptInfo);
 
+// Creates text file with $title as the name
 $myfile = fopen("/home/student/cst127/Receipts/$title.txt", "w");
 
+// Loops through items in the cart and prints it in rows to the text file
 while($row = mysqli_fetch_array($receiptQuery)){
 	$receiptProductId = $row['product_id'];
 	$receiptQuantity = $row['quantity'];
@@ -28,14 +31,14 @@ while($row = mysqli_fetch_array($receiptQuery)){
 	fwrite($myfile, $receiptText);
 
 }
+// Adds total cost to the bottom of the text file and closes the file
 $totalTaxText = "Total tax: $" . number_format($totalTax, 2) . "\n";
 $totalText = "Total: $" . number_format($total, 2);
 fwrite($myfile, $totalTaxText);
 fwrite($myfile, $totalText);
 fclose($myfile);
 
-/*
-$myfile = fopen("/home/student/cst127/Receipts/$title.txt", "w");
-fclose($myfile);
-*/
+
+header('location:index.php');
+
 ?>
