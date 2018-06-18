f23a791685081157020dd2f37044c8f17fc54657
David Hicks <davidhicks14@gmail.com>
2018-06-18 12:27:20 -0700

    cart updated to show correct totals

--- web/cart.php
+++ web/cart.php
@@ -95,7 +95,7 @@
 					 $tax = number_format($tax, 2);
 					 $subTotal = (($row['price']*$row['quantity'])+$tax);
 					 $rowQuantity = $row['quantity'];
-					 $total += $subTotal;
+                     $total += $subTotal;
 					 $totalTax += $tax;
                      echo "<tr>";
                      echo "<td align='center'>" . $row['name'] . "</td>";
@@ -140,6 +140,9 @@
             <?php require_once('./config.php'); ?>
 
             <form action="charge.php" method="post">
+            <input type="checkbox" id="billaddr" name="billaddr" onClick="billAddrFunction();"> Billing address different than shipping address. <br>
+            <input type='text' id='billingaddress' pattern="[\sa-zA-Z0-9]{4,20}" name="billingaddress" style="visibility:hidden;">
+            
             <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
                     data-key="<?php echo $stripe['publishable_key']; ?>"
                     data-description="Payment Form"
@@ -150,10 +153,16 @@
     </article>
     </body>
 
-	 
-
     </article>
-
+<script>
+function billAddrFunction() {
+    if(document.getElementById('billaddr').checked){
+        document.getElementById('billingaddress').style.visibility = 'visible';
+    }else{
+        document.getElementById('billingaddress').style.visibility = 'hidden';
+    }
+}
+</script>
 	
     <?php
 	// This checks if the admin is logged in and allows them to 
